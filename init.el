71e249c6879838b9e288220792c56749e6fe83c8
sensune <sensune@outlook.com>
2019-04-18 20:50:54 +0800

    update

--- init.el
+++ init.el
@@ -2,16 +2,6 @@
 (setq inhibit-splash-screen t)
 ;;禁止自动备份
 (setq make-backup-files nil)
-(add-to-list 'load-path "~/.emacs.d/themes")
-(require 'autothemer)
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'gruvbox t)
-
-;; Setting English Font
-(set-face-attribute
-   'default nil :font "Monaco 11")
-;; Setting Chinese Font
-(dolist (charset '(kana han symbol cjk-misc bopomofo))
-    (set-fontset-font (frame-parameter nil 'font)
-		                  charset
-				              (font-spec :family "Microsoft Yahei" :size 14)))
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(package-initialize)
