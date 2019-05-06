bb89b2eae88296ac0d866989f527e49ba96e40cd
Sensune <sensune@outlook.com>
2019-05-06 06:58:33 +0000

    update

--- init.el
+++ init.el
@@ -5,3 +5,18 @@
 (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (package-initialize)
+
+(setq-default auto-save-timeout 15) ; 15秒无动作,自动保存
+(setq-default auto-save-interval 50) ; 100个字符间隔, 自动保存
+
+;;安装use-package插件
+(unless (package-installed-p 'use-package)
+(package-refresh-contents)
+(package-install 'use-package))
+
+;;安装go-mode
+(use-package go-mode
+  :ensure t
+  :mode (("\\.go\\'" . go-mode))
+  :hook ((before-save . gofmt-before-save))
+  )
